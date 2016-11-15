43cda019aba1448d974a23430041250d88a18b31
Kyusun Kim <maniagoon@gmail.com>
2016-11-15 20:24:25 +0900

    Make PackageThree extends PackageFour

--- lib/src/beaver_demo_package_3_base.dart
+++ lib/src/beaver_demo_package_3_base.dart
@@ -1,4 +1,6 @@
-class PackageThree {
+import 'package:beaver_demo_package_4/beaver_demo_package_4.dart';
+
+class PackageThree extends PackageFour {
   @override
   String toString() => 'package 3';
 }
